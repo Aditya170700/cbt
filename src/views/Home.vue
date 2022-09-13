1a34b944f3918ba66593cc1f08a32ce2301ad7ae
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2022-09-13 12:30:06 +0700

    Home, Font family

---
+++ src/views/Home.vue
@@ -0,0 +1,101 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<!-- eslint-disable no-undef -->
+<script setup>
+import { ref } from "vue";
+import backgroundImage from "@/assets/img/static/bg-landing.jpeg";
+
+let widthContent = window.innerWidth;
+let heightContent = window.innerHeight;
+let minus = widthContent > 900 ? 90 : 70;
+let minHeight = ref((heightContent - minus) / 2);
+</script>
+
+<template>
+  <b-container fluid class="p-0">
+    <b-navbar variant="faded" type="light" class="shadow-sm">
+      <b-navbar-brand class="mb-0">
+        <div class="d-flex align-items-center">
+          <img src="./../assets/img/logo.png" alt="Logo" style="width: 15%" />
+          <div class="small">
+            <h6 class="small fw-bold mb-0">PPSDM</h6>
+            <span class="small mt-0">Perhubungan Laut</span>
+          </div>
+        </div>
+      </b-navbar-brand>
+      <div class="d-none d-lg-block">
+        <b-button variant="outline-primary" size="sm" class="rounded-3 mx-1"
+          >All Test</b-button
+        >
+        <b-button variant="outline-primary" size="sm" class="rounded-3 mx-1"
+          >LSP</b-button
+        >
+        <b-button variant="outline-primary" size="sm" class="rounded-3 mx-1"
+          >Login</b-button
+        >
+        <b-button variant="outline-primary" size="sm" class="rounded-3 mx-1"
+          >Sign Up</b-button
+        >
+      </div>
+      <div class="d-lg-none">
+        <i class="fas fa-bars fa-2x text-primary"></i>
+      </div>
+    </b-navbar>
+    <div :style="`min-height:${minHeight}px;`" class="position-relative">
+      <div class="d-none d-lg-block p-lg-5">
+        <div class="h1" style="font-size: 42px">CBT PPSDM</div>
+        <div class="h2">Perhubungan Laut</div>
+      </div>
+      <div class="d-none d-lg-block p-lg-5">
+        <b-card class="position-absolute rounded-4 border-0 shadow-lg">
+          <b-card-text class="badge bg-light text-dark">
+            <i class="fas fa-circle-check me-2"></i>Explore Available Test
+          </b-card-text>
+
+          <b-form>
+            <b-row>
+              <b-col lg="12" class="mb-3">
+                <label for="">Name</label>
+                <b-form-input size="lg" placeholder="XYZ"></b-form-input>
+              </b-col>
+              <b-col lg="12" class="mb-3 text-center">
+                <b-button variant="outline-primary" class="rounded-5 px-4"
+                  >SEARCH</b-button
+                >
+              </b-col>
+            </b-row>
+          </b-form>
+        </b-card>
+      </div>
+      <div class="position-absolute d-lg-none" style="top: 30%">
+        <div class="text-center mb-5">
+          <div class="h1" style="font-size: 42px">CBT PPSDM</div>
+          <div class="h2">Perhubungan Laut</div>
+        </div>
+        <div class="px-3">
+          <b-card class="rounded-4 border-0 shadow-lg">
+            <b-card-text class="badge bg-light text-dark">
+              <i class="fas fa-circle-check me-2"></i>Explore Available Test
+            </b-card-text>
+
+            <b-form>
+              <b-row>
+                <b-col lg="12" class="mb-3">
+                  <label for="">Name</label>
+                  <b-form-input size="lg" placeholder="XYZ"></b-form-input>
+                </b-col>
+                <b-col lg="12" class="mb-3 text-center">
+                  <b-button variant="outline-primary" class="rounded-3 px-4"
+                    >Search</b-button
+                  >
+                </b-col>
+              </b-row>
+            </b-form>
+          </b-card>
+        </div>
+      </div>
+    </div>
+    <div
+      :style="`min-height:${minHeight}px; background-image: url('${backgroundImage}'); bacground-size: cover; background-position: center; background-repeat: no-repeat; background-fit: cover;`"
+    ></div>
+  </b-container>
+</template>
