c4f2b559d3fea7102b4a04cb921b875ec788acee
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2022-11-22 19:18:52 +0700

    Base url

--- src/stores/app.js
+++ src/stores/app.js
@@ -3,9 +3,12 @@ import { defineStore } from "pinia";
 
 export const appStore = defineStore("app", {
   state: () => ({
-    baseurl: process.env.NODE_ENV == "development" ? "http://localhost:8000/api/" : "https://api.example.com",
-    urlCbt: process.env.NODE_ENV == "development" ? "http://localhost:5174/" : "https://cbt.com",
-    urlSimpel: process.env.NODE_ENV == "development" ? "http://localhost:5173/" : "https://simpel.com",
+    // baseurl: process.env.NODE_ENV == "development" ? "http://localhost:8000/api/" : "https://api.example.com",
+    // urlCbt: process.env.NODE_ENV == "development" ? "http://localhost:5174/" : "https://cbt.com",
+    // urlSimpel: process.env.NODE_ENV == "development" ? "http://localhost:5173/" : "https://simpel.com",
+    baseurl: "https://api-simple.smartmanagement.id/api/",
+    urlCbt: "https://cbt-simple.smartmanagement.id/",
+    urlSimpel: "https://simple.smartmanagement.id/",
     token: localStorage.getItem("token") || null,
     user: localStorage.getItem("user") || null,
     reload: false,
