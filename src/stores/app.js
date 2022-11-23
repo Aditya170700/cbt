6f8977e322a65223518ddab2d54b629a9eba91bf
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2022-11-23 09:12:02 +0700

    Base url dll

--- src/stores/app.js
+++ src/stores/app.js
@@ -3,12 +3,12 @@ import { defineStore } from "pinia";
 
 export const appStore = defineStore("app", {
   state: () => ({
-    // baseurl: process.env.NODE_ENV == "development" ? "http://localhost:8000/api/" : "https://api.example.com",
-    // urlCbt: process.env.NODE_ENV == "development" ? "http://localhost:5174/" : "https://cbt.com",
-    // urlSimpel: process.env.NODE_ENV == "development" ? "http://localhost:5173/" : "https://simpel.com",
-    baseurl: "https://api-simple.smartmanagement.id/api/",
-    urlCbt: "https://cbt-simple.smartmanagement.id/",
-    urlSimpel: "https://simple.smartmanagement.id/",
+    baseurl: process.env.NODE_ENV == "development" ? "http://localhost:8000/api/" : "https://api-simple.smartmanagement.id/api/",
+    urlCbt: process.env.NODE_ENV == "development" ? "http://localhost:5174/" : "https://cbt-simple.smartmanagement.id/",
+    urlSimpel: process.env.NODE_ENV == "development" ? "http://localhost:5173/" : "https://simple.smartmanagement.id/",
+    // baseurl: "https://api-simple.smartmanagement.id/api/",
+    // urlCbt: "https://cbt-simple.smartmanagement.id/",
+    // urlSimpel: "https://simple.smartmanagement.id/",
     token: localStorage.getItem("token") || null,
     user: localStorage.getItem("user") || null,
     reload: false,
