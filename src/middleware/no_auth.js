d98a1f2d72f6faadda8e2c26b104888c4cce168f
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2022-11-24 17:54:07 +0700

    State in form login

--- src/middleware/no_auth.js
+++ src/middleware/no_auth.js
@@ -1,7 +1,40 @@
 export default function no_auth({ next, router }) {
-  if (localStorage.getItem("token") && localStorage.getItem("user")) {
-    return router.go(-1);
+  if ((localStorage.getItem("tokenPanitia") && localStorage.getItem("userPanitia"))) {
+    router.push({
+      name: "dashboard-panitia",
+    });
+    return;
   }
+
+  if ((localStorage.getItem("tokenPeserta") && localStorage.getItem("userPeserta"))) {
+    router.push({
+      name: "peserta",
+    });
+    return;
+  }
+
+  if ((localStorage.getItem("tokenInstruktur") && localStorage.getItem("userInstruktur"))) {
+    router.push({
+      name: "instruktur",
+    });
+    return;
+  }
+
+  if ((localStorage.getItem("tokenAdminLemdik") && localStorage.getItem("userAdminLemdik"))) {
+    router.push({
+      name: "dashboard-non-pnbp-admin-lemdik",
+    });
+    return;
+  }
+
+  if ((localStorage.getItem("tokenAdministratorPelatihan") && localStorage.getItem("userAdministratorPelatihan"))) {
+    router.push({
+      name: "dashboard-administrator",
+    });
+    return;
+  }
+
+  localStorage.clear();
 
   return next();
 }
