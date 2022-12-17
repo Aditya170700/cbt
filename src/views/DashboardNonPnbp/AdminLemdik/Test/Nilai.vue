c0e710b14542f3046c6a4cb66963dc9d26859087
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2022-12-18 00:17:27 +0700

    1.	Informasi Kegiatan diklat, dan waktu ditampilkan di pengisian nilai

--- src/views/DashboardNonPnbp/AdminLemdik/Test/Nilai.vue
+++ src/views/DashboardNonPnbp/AdminLemdik/Test/Nilai.vue
@@ -69,6 +69,52 @@ function fetchData() {
             </div>
           </div>
         </div>
+        <div class="row px-2 mb-2" v-if="!result.loading">
+          <div class="col-12">
+            <div class="row">
+              <div class="col-lg-2 col-3 fw-bold">Jenis Diklat</div>
+              <div class="col-lg-10 col-9">
+                {{ result.data?.test?.parent }}
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-2 col-3 fw-bold">Kategori Diklat</div>
+              <div class="col-lg-10 col-9">
+                {{ result.data?.test?.jenis_diklat }}
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-2 col-3 fw-bold">Program Diklat</div>
+              <div class="col-lg-10 col-9">
+                {{ result.data?.test?.referensi_diklat }}
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-2 col-3 fw-bold">Tanggal</div>
+              <div class="col-lg-10 col-9">
+                {{ result.data?.test?.tgl }}
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-2 col-3 fw-bold">Mulai</div>
+              <div class="col-lg-10 col-9">
+                {{ result.data?.test?.mulai }}
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-2 col-3 fw-bold">Selesai</div>
+              <div class="col-lg-10 col-9">
+                {{ result.data?.test?.selesai }}
+              </div>
+            </div>
+            <div class="row mt-3">
+              <div
+                class="col-lg-12 col-12"
+                v-html="result.data?.test?.deskripsi"
+              ></div>
+            </div>
+          </div>
+        </div>
         <div class="row px-2">
           <div class="table-responsive">
             <table class="table table-bordered">
