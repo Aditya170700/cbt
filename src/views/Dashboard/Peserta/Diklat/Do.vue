5fdecce877be92b72112b8b17cb2c2ea783cc721
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2022-12-17 22:45:45 +0700

    13.	Klik kanan di disable

--- src/views/Dashboard/Peserta/Diklat/Do.vue
+++ src/views/Dashboard/Peserta/Diklat/Do.vue
@@ -31,6 +31,13 @@ let result = reactive({
 });
 
 onMounted(() => {
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
+  document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+      return false;
+    }
+  };
+
   fetchData();
 });
 
