70ca5b2cc039cafb0aa4591313418e071d173044
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2022-12-20 15:45:22 +0700

    Debug 5

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -2,15 +2,48 @@
 <script setup>
 import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
-import { default as BlotFormatter } from "quill-blot-formatter";
+import axios from "axios";
+// import { default as BlotFormatter } from "quill-blot-formatter";
+import ImageUploader from "quill-image-uploader";
 import { ref } from "vue";
+import { appStore } from "@/stores/app";
+import { clearBase64 } from "@/assets/js/utils";
 
 let quill = ref(null);
 let input = ref("");
-const blotFormatterModule = {
-  name: "blotFormatter",
-  module: BlotFormatter,
-  options: {},
+const storeApp = appStore();
+// const blotFormatterModule = {
+//   name: "blotFormatter",
+//   module: BlotFormatter,
+//   options: {},
+// };
+let modules = {
+  name: "imageUploader",
+  module: ImageUploader,
+  options: {
+    upload: (file) => {
+      // convert file to base64 with promise
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = (e) => {
+          axios
+            .post(`${storeApp.baseurl}quill/image-uploader`, {
+              image: clearBase64(e.target.result),
+              ext: file.name.split(".").pop().toLowerCase(),
+            })
+            .then((res) => {
+              if (res.data.status != 200) throw new Error(res.data.message);
+              resolve(res.data.data);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        };
+        reader.onerror = (error) => reject(error);
+      });
+    },
+  },
 };
 </script>
 
@@ -22,7 +55,7 @@ const blotFormatterModule = {
     contentType="html"
     style="height: 300px"
     toolbar="full"
-    :modules="blotFormatterModule"
+    :modules="modules"
   />
   <div class="mt-5 ql-editor" v-html="input"></div>
 </template>
