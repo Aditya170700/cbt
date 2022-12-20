933518709b8c53f63dcb5c5c8fd89c468730f225
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2022-12-20 16:52:16 +0700

    Debug 14

--- src/stores/app.js
+++ src/stores/app.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import { defineStore } from "pinia";
-import BlotFormatter from "quill-blot-formatter/dist/BlotFormatter.js";
+import BlotFormatter from "quill-blot-formatter";
 import ImageUploader from "quill-image-uploader";
 import { clearBase64 } from "@/assets/js/utils";
 import axios from "axios";
